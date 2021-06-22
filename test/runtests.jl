77d6e4dd9ee8fd40b52e36f7e02eff0ab7af83ee
Bart Janssens <bart@bartjanssens.org>
2021-06-23 00:27:56 +0200

    Integrate code to run examples

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
+using Base: process_status
 using Test
 
 import QML
 using Documenter: doctest
 
-excluded = ["runtests.jl", "qml", "include"]
+excluded = ["runtests.jl", "qml", "include", "runexamples.jl"]
 
 testfiles = filter(fname -> fname ∉ excluded, readdir(@__DIR__))
 
@@ -16,7 +17,54 @@ end
 
 doctest(QML, fix=true)
 
-if !Sys.iswindows() #Skip on Windows because OpenGL doesn't work on Github CI
+function runexamples()
+  ENV["QT_LOGGING_RULES"] = "qt.scenegraph.time.renderloop=true;"
+
+  renderstring = "Frame rendered"
+
+  function errorfilter(line)
+    return !contains(line, renderstring)
+  end
+
+  for fname in readdir()
+    if endswith(fname, ".jl") && fname ∉ excluded
+      if any(contains.(readlines(fname),"exec_async"))
+        println("Skipping async example $fname")
+        continue
+      end
+      println("running example ", fname, "...")
+      outbuf = IOBuffer()
+      errbuf = IOBuffer()
+      testproc = run(pipeline(`$(Base.julia_cmd()) --project $fname`; stdout=outbuf, stderr=errbuf); wait = false)
+      current_time = 0.0
+      timestep = 0.1
+      errstr = ""
+      rendered = false
+      while process_running(testproc)
+        sleep(timestep)
+        current_time += timestep
+        errstr *= String(take!(errbuf))
+        rendered = contains(errstr, renderstring)
+        if current_time >= 300.0 || rendered
+          sleep(0.5)
+          kill(testproc)
+          break
+        end
+      end
+      outstr = String(take!(outbuf))
+      errlines = join(filter(errorfilter, split(errstr, r"[\r\n]+")), "\n")
+      if !rendered || contains(lowercase(errlines), "error")
+        throw(ErrorException("Example $fname errored with output:\n$outstr\nand error:\n$errlines"))
+      elseif isempty(outstr) && isempty(errlines)
+        println("Example $fname finished")
+      elseif isempty(outstr)
+        println("Example $fname finished with error:\n$errlines")
+      else
+        println("Example $fname finished with output:\n$outstr")
+      end
+    end
+  end
+end
 
 import LibGit2
 
@@ -35,11 +83,9 @@ withenv("JULIA_LOAD_PATH" => nothing, "JULIA_GR_PROVIDER" => "BinaryBuilder") do
           pkg"status"
         """
         run(`$(Base.julia_cmd()) --project -e "$updatecommand"`)
-        run(`$(Base.julia_cmd()) --project runexamples.jl`)
+        runexamples()
       end
     end
     println(pwd())
   end
-end
-
 end
